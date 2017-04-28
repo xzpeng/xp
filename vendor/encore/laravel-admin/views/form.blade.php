82a441ca38e947244037957576ce0d57bd2fd883
xzpeng <pengxiaozhou2008@gmail.com>
2017-04-28 20:12:01 +0800

    修改vender tree

---
+++ vendor/encore/laravel-admin/views/form.blade.php
@@ -0,0 +1,57 @@
+<div class="box box-info">
+    <div class="box-header with-border">
+        <!-- <h3 class="box-title">{{ $form->title() }}</h3> -->
+
+        <div class="box-tools">
+            {!! $form->renderHeaderTools() !!}
+        </div>
+    </div>
+    <!-- /.box-header -->
+    <!-- form start -->
+    {!! $form->open(['class' => "form-horizontal"]) !!}
+        <div class="box-body">
+
+            @if(!$tabObj->isEmpty())
+                @include('admin::form.tab', compact('tabObj'))
+            @else
+                <div class="fields-group">
+                    @foreach($form->fields() as $field)
+                        @if( ! $field instanceof \Encore\Admin\Form\Field\HasMany)
+                            {!! $field->render() !!}
+                        @endif
+                    @endforeach
+                </div>
+                <div class="fields-group">
+                    @foreach($form->fields() as $field)
+                        @if( $field instanceof \Encore\Admin\Form\Field\HasMany)
+                            {!! $field->render() !!}
+                        @endif
+                    @endforeach
+                </div>
+            @endif
+
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="col-sm-{{$width['label']}}">
+
+            </div>
+            <div class="col-sm-{{$width['field']}}">
+
+                {!! $form->submitButton() !!}
+
+                {!! $form->resetButton() !!}
+
+            </div>
+
+        </div>
+
+        @foreach($form->getHiddenFields() as $hiddenField)
+            {!! $hiddenField->render() !!}
+        @endforeach
+
+        <!-- /.box-footer -->
+    {!! $form->close() !!}
+</div>
+
---
+++ vendor/encore/laravel-admin/views/form/file.blade.php
@@ -0,0 +1,14 @@
+<div class="form-group {!! !$errors->has($errorKey) ?: 'has-error' !!}">
+
+    <label for="{{$id}}" class="col-sm-{{$width['label']}} control-label">{{$label}}</label>
+
+    <div class="col-sm-{{$width['field']}}">
+
+        @include('admin::form.error')
+        <input type="file" class="{{$class}}" name="{{$name}}{{$multiple ? '[]' : ''}}" {!! $attributes !!} />
+        <input type="hidden" class="{{$class}}_action" name="{{$name}}_action" value="0"/>
+
+        @include('admin::form.help-block')
+
+    </div>
+</div>
